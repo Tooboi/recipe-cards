9e50b390c3eb29846ffc4fad58b9896cae36d4ef
Tooboi <japica123@gmail.com>
2025-05-20 02:13:22 -0400

    New Card page added

---
+++ src/app/new-card/page.tsx
@@ -0,0 +1,13 @@
+export const metadata = {
+  title: 'New Card',
+  caption: 'Create a new recipe card',
+};
+
+export default function NewCard() {
+
+  return (
+    <div className="flex flex-col items-center">
+      <div className="max-w-8xl mx-auto min-h-screen pb-6 pt-4 md:px-4 lg:px-10"></div>
+    </div>
+  );
+}
