b6b6ac97aad87e84f294b10fec1de75e19cf989f
Tooboi <japica123@gmail.com>
2025-05-24 02:59:26 -0400

    clerk work3

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import mongoose, { Mongoose } from 'mongoose';
 
-const MONGODB_URL = process.env.MONGODB_URL!;
+const MONGODB_URL = process.env.DIRECT_DATABASE_URL!;
 
 interface MongooseConn {
   conn: Mongoose | null;
