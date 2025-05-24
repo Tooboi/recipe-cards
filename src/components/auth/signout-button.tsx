a4410c447edeba733a6b72d585ee58c0d6a473bf
Tooboi <japica123@gmail.com>
2025-05-23 23:28:07 -0400

    purged red

--- src/components/auth/signout-button.tsx
+++ src/components/auth/signout-button.tsx
@@ -1,11 +1,11 @@
-import { signOut } from '../../lib/auth';
+// import { signOut } from '../../lib/auth';
 
 export default function SignOutButton() {
   return (
     <form
       action={async () => {
         'use server';
-        await signOut();
+        // await signOut();
       }}
     >
       <button className='font-semibold' type="submit">SIGN OUT</button>
