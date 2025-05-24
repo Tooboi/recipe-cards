979c9e361a6d1ea8d0f7e2b4a9e5458ad777b0be
Tooboi <japica123@gmail.com>
2025-05-24 03:34:17 -0400

    clerk work6

--- src/app/api/webhooks/clerk/route.ts
+++ src/app/api/webhooks/clerk/route.ts
@@ -3,8 +3,8 @@ import { clerkClient, WebhookEvent } from "@clerk/nextjs/server";
 import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 import { Webhook } from "svix";
-import { createUser } from '@/app/actions';
-// import { createUser } from "@/lib/actions/user.actions";
+// import { createUser } from '@/app/actions';
+import { createUser } from "@/lib/actions/user.actions";
 export async function POST(req: Request) {
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
   const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
@@ -31,6 +31,9 @@ export async function POST(req: Request) {
   // Get the body
   const payload = await req.json();
   const body = JSON.stringify(payload);
+
+  console.log(body);
+  
 
   // Create a new Svix instance with your secret.
   const wh = new Webhook(WEBHOOK_SECRET);
@@ -59,6 +62,7 @@ export async function POST(req: Request) {
   if (eventType === "user.created") {
     const { id, email_addresses, image_url, username } =
       evt.data;
+console.log();
 
     const user = {
       clerkId: id,
@@ -75,7 +79,7 @@ export async function POST(req: Request) {
     if (newUser) {
        client.users.updateUserMetadata(id, {
         publicMetadata: {
-          userId: newUser.id,
+          userId: newUser._id,
         },
       });
     }
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,12 @@
-import { clerkMiddleware } from '@clerk/nextjs/server';
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-export default clerkMiddleware();
+const isPublicRoute = createRouteMatcher(['/', 'forgot-password', 'api/webhooks/clerk', '/sign-in(.*)', '/sign-up(.*)']);
+
+export default clerkMiddleware(async (auth, req) => {
+  if (!isPublicRoute(req)) {
+    await auth.protect();
+  }
+});
 
 export const config = {
   matcher: [
@@ -9,5 +15,5 @@ export const config = {
     // Always run for API routes
     '/(api|trpc)(.*)',
   ],
-  publicRoutes: ['/', 'forgot-password', 'api/webhooks/clerk'],
+  // publicRoutes: ['/', 'forgot-password', 'api/webhooks/clerk'],
 };
