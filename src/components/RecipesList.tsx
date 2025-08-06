b8d2144f428253a6fb2dac8892b22423491456e2
Josh Pica <japica123@gmail.com>
2025-08-06 16:38:05 -0400

    more visual fixes

--- src/components/RecipesList.tsx
+++ src/components/RecipesList.tsx
@@ -38,24 +38,23 @@ interface RecipesListProps {
 
 export default function RecipesList({ initialRecipes }: RecipesListProps) {
   return (
-    <div className="shadow-sm bg-white overflow-hidden border-0 pt-0">
-      <div className="bg-slate-50 border-b px-6 py-5 rounded-none">
-        <div className="text-xl text-slate-900">Recipes</div>
+    <div className="overflow-hidden">
+      <div className="px-6 pt-5 rounded-none">
+        <div className="text-xl text-center mx-auto text-slate-900 w-full font-semibold">Explore Recent Recipes</div>
       </div>
-      <div className="p-6">
+      <div className="pt-2">
         {initialRecipes.length === 0 ? (
           <div className="text-center py-10 text-slate-500 italic">No recipes found. Create one now!</div>
         ) : (
-          <div className="grid grid-cols-3 gap-4">
+          <div className="grid grid-cols-2 sm:grid-cols-3 gap-4">
             {initialRecipes.map((recipe) => (
               <Link href={`/recipes/${recipe.id}`} key={recipe.id} className="block col-span-1">
-                <div className="p-4 border border-slate-200 rounded-lg hover:bg-slate-50 transition-colors group">
+                <div className="px-4 py-2 border border-slate-600 rounded-lg bg-slate-300 hover:bg-slate-100 transition-colors group">
                   <div className="font-medium text-lg text-slate-900 group-hover:text-slate-700">{recipe.title || 'Recipe'}</div>
-                  <div className="text-sm text-slate-600 mt-1">{formatDate(recipe.updatedAt)}</div>
-                  <div className="text-sm text-slate-600 mt-1">{recipe.user.username}</div>
-
-                  <div className="mt-3 flex items-center gap-4 text-xs text-slate-500">
-                    <div className="flex items-center bg-slate-100 px-3 py-1 rounded-full">
+                  <div className="text-sm text-slate-600">{recipe.user.username}</div>
+                  <div className="text-xs text-slate-600">{formatDate(recipe.updatedAt)}</div>
+                  <div className="mt-3 flex items-center gap-4 text-xs text-slate-600">
+                    <div className="flex items-center bg-slate-100 border border-slate-400 px-3 py-1 rounded-full">
                       <span className="font-medium mr-1">Ingredients:</span>
                       <span className="text-slate-600 font-medium">{recipe.ingredients.length}</span>
                     </div>
